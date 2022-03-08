40f1655f024509ea52e32d033514b6b33a3c0770
jungwoo <westhyena@gmail.com>
2022-03-08 18:07:05 +0900

    feat(setup): use build py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from distutils.command.build_py import build_py
 import os
 import sys
 import site
@@ -8,13 +9,13 @@ from setuptools import (
     Extension
 )
 from setuptools.command.install import install
+from distutils.command.build_py import build_py
 
 
 PACKAGE = "fisher"
 
 
-class AfterInstall(install):
-
+class AfterBuildPy(build_py):
   def build_c(self, path, filename):
     obj_path = os.path.join(path, PACKAGE, "src", f"{filename}.so")
     c_path = os.path.join(path, PACKAGE, "src", f"{filename}.c")
@@ -25,16 +26,9 @@ class AfterInstall(install):
       raise Exception("gcc build failed.")
 
   def run(self):
-    install.run(self)
+    build_py.run(self)
 
-    if "--user" in sys.argv:
-      installed_path = site.getusersitepackages()
-    else:
-      site_path_list = site.getsitepackages()
-      for site_path in site_path_list:
-        if (os.path.exists(os.path.join(site_path, PACKAGE))):
-          installed_path = site_path
-
+    installed_path = self.build_lib
     self.build_c(installed_path, "fisher")
     self.build_c(installed_path, "asa159")
 
@@ -46,9 +40,9 @@ setup(
     ],
     test_suite="tests",
     tests_require=["pytest"],
-    packages=find_packages(exclude="tests"),
+    packages=["fisher"],
     package_data={
-      PACKAGE: ["src/*.c"]
+      PACKAGE: ["src/*.c", "src/asa159.so", "src/fisher.so"]
     },
-    cmdclass={"install": AfterInstall},
+    cmdclass={"build_py": AfterBuildPy},
 )
