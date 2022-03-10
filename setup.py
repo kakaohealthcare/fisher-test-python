e0ddee9517db0f58589e5d8143a8a874f5e1ec82
westhyena <westhyena@gmail.com>
2022-03-10 09:57:04 +0900

    Issue28.build when install (#29)
    * feat(setup): build c library when install
    
    * feat(setup): build c when install
    
    * feat(setup): use build py
    
    * chore(setup): remove unusing imports

--- setup.py
+++ setup.py
@@ -1,4 +1,28 @@
-from setuptools import setup, find_packages
+import os
+
+from setuptools import setup
+from distutils.command.build_py import build_py
+
+
+PACKAGE = "fisher"
+
+
+class AfterBuildPy(build_py):
+  def build_c(self, path, filename):
+    obj_path = os.path.join(path, PACKAGE, "src", f"{filename}.so")
+    c_path = os.path.join(path, PACKAGE, "src", f"{filename}.c")
+    ret = os.system(
+        f"gcc -o {obj_path} -shared -fPIC {c_path}"
+    )
+    if ret != 0:
+      raise Exception("gcc build failed.")
+
+  def run(self):
+    build_py.run(self)
+
+    installed_path = self.build_lib
+    self.build_c(installed_path, "fisher")
+    self.build_c(installed_path, "asa159")
 
 
 setup(
@@ -8,8 +32,9 @@ setup(
     ],
     test_suite="tests",
     tests_require=["pytest"],
-    packages=find_packages(),
+    packages=["fisher"],
     package_data={
-      "fisher": ["src/*.so"]
-    }
+      PACKAGE: ["src/*.c", "src/asa159.so", "src/fisher.so"]
+    },
+    cmdclass={"build_py": AfterBuildPy},
 )
